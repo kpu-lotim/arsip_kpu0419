c7a02c72e9589c5a3eaa87f7fbc51266a36dabb9
kpu-lotim <majikongo@gmail.com>
2026-06-27 00:09:48 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+// Ganti sementara dengan kode ini untuk membuang semua cache
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => caches.delete(cache))
+            );
+        })
+    );
+});
